1c3fbff7833ae652b94faf94fef8aab61a397c5d
Ram Goli <ramsgoli@gmail.com>
2018-09-17 16:08:51 -0700

    feat(navbar): add color to active navbar link

--- src/navbar/Link.js
+++ src/navbar/Link.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { RowItem } from '../lib'
-import { Link as _Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Link = ({ children, to }) => {
   return (
     <RowItem style={{ margin: '0 10px' }}>
-      <_Link to={to}>{children}</_Link>
+      <NavLink to={to} exact activeStyle={{ color: '#F8CC29' }}>
+        {children}
+      </NavLink>
     </RowItem>
   )
 }
