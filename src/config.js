9e91369300457e889a8011faeb4b636be86b247d
Ram Goli <ramsgoli@gmail.com>
2018-09-17 17:18:52 -0700

    feat(): add medium link

--- src/config.js
+++ src/config.js
@@ -19,8 +19,25 @@ export default {
       url: 'mailto:hello@ucladevx.com'
     },
     {
+      path: 'medium.svg',
+      url: 'https://www.medium.com/ucladevx'
+    },
+    {
       path: 'github.svg',
       url: 'https://www.github.com/ucladevx/'
+    }
+  ],
+  team: [
+    {
+      title: 'Executive Board',
+      people: [
+        {
+          name: 'Ram Goli',
+          role: 'President',
+          linkedin: 'https://linkedin.com/in/ramsgoli',
+          email: 'mailto::ramsgoli@ucla.edu'
+        }
+      ]
     }
   ]
 }
