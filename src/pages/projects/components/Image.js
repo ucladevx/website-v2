21f32e0bfb185c5884fb418ce77e4ecb75592b89
Ram Goli <ramsgoli@gmail.com>
2018-09-21 14:51:46 -0700

    feat(projects): add white background to images (MOBILE)

--- src/pages/projects/components/Image.js
+++ src/pages/projects/components/Image.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Image = ({ path }) => {
-  const Img = require(`images/productLogos/${path}`)
+  const Img = require(`images/productLogos/${path}`) // eslint-disable-line
   return (
-    <div className="project_image">
-      <img src={Img} />
-    </div>
+    <div className="project_image" style={{ backgroundImage: `url(${Img})` }} />
   )
+}
+
+Image.propTypes = {
+  path: PropTypes.string.isRequired
 }
 
 export default Image
