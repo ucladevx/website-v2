f5a51330eef787830775deb9dbdfdd1142e08583
Terrence Ho <terrenceho.books@gmail.com>
2020-01-03 18:05:28 -0800

    Added new projects

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,25 +1,41 @@
 import bQuestLogo from '../assets/images/productLogos/bquest.png'
 import bruinBiteLogo from '../assets/images/productLogos/bruin_bite.png'
-import bruinHubLogo from '../assets/images/productLogos/bruin_hub.png'
-// import mappeningLogo from '../assets/images/productLogos/mappening.png'
-import studySmartLogo from '../assets/images/productLogos/study_smart.png'
+// import bruinHubLogo from '../assets/images/productLogos/bruin_hub.png'
+import bmapsLogo from '../assets/images/productLogos/bmaps.png'
+import studyBLogo from '../assets/images/productLogos/studyb.png'
 import onCampusLogo from '../assets/images/productLogos/oncampus.png'
 import quickeyLogo from '../assets/images/productLogos/quickey.png'
+import broomieLogo from '../assets/images/productLogos/broomie.svg'
+import drugDeciderLogo from '../assets/images/productLogos/drugdecider.jpg'
 
 export default {
   spring18: [
     {
+      name: 'BMaps',
+      description:
+        'BMaps is a web app that aggregates events across the UCLA campus onto a single highly visual map to minimize search effort and promote discovery. Our app allows students to browse through events happening near or on campus and explore everything UCLA has to offer.',
+      link: 'https://bmaps.io/',
+      image: bmapsLogo
+    },
+    {
+      name: 'Broomie',
+      description:
+        'Broomie helps improve roommate communication. Get notified when you need to complete a chore. Assign rotational chores to all roommates. Keep track of communal shopping lists.',
+      image: broomieLogo
+    },
+    {
       name: 'Bruin Bite',
       description:
         'Bringing Bruins together one bite at a time!\n\nWith the ease of a few button pushes, Bruin Bite, matches you with new people to eat with at the dining halls! Our goal is to bring you an app that makes meeting fellow Bruins effortless.',
+      link: 'https://apps.apple.com/us/app/bruin-bite/id1388811699',
       image: bruinBiteLogo
     },
-    {
-      name: 'BruinHub',
-      description:
-        'BruinHub is a personalizable web app designed to help Bruins weather the challenges of the UCLA quarter system. Coupled with our chrome extension and event scheduler, we aim to empower you to get the most out of your quarter',
-      image: bruinHubLogo
-    },
+    // {
+    //   name: 'BruinHub',
+    //   description:
+    //     'BruinHub is a personalizable web app designed to help Bruins weather the challenges of the UCLA quarter system. Coupled with our chrome extension and event scheduler, we aim to empower you to get the most out of your quarter',
+    //   image: bruinHubLogo
+    // },
     {
       name: 'Bquest',
       description:
@@ -27,13 +43,17 @@ export default {
       link: 'https://bquest.ucladevx.com/',
       image: bQuestLogo
     },
-    // {
-    //   name: 'Mappening',
-    //   description:
-    //     'Mappening is a web app that aggregates events across the UCLA campus onto a single highly visual map to minimize search effort and promote discovery. Our app allows students to browse through events happening near or on campus and explore everything UCLA has to offer.',
-    //   link: 'https://www.mappening.io/',
-    //   image: mappeningLogo
-    // },
+    {
+      name: 'DrugDecider',
+      description:
+        'DrugDecider uses Machine Learning to help patients and clinicians evaluate and predict the effects of certain psychiatric medications on specific patients.',
+      image: drugDeciderLogo
+    },
+    {
+      name: 'Pulp',
+      description:
+        'Curated Experiences Nearby\n\nPulp provides personalized recommendations to help you find your next destination. Designed for all kinds of travelers from all walks of life, finding things to do in an old or new city is easy with Pulp.'
+    },
     {
       name: 'Quickey',
       description:
@@ -49,11 +69,21 @@ export default {
       image: onCampusLogo
     },
     {
-      name: 'StudySmart',
+      name: 'StudyB',
       description:
-        "StudySmart gives students instant access to all the information they need about the best study spots on campus. Whether it's how busy a library is, what time the Hedrick Study closes, or where to reserve a YRL pod, all the answers are found in a single application that is updated in real-time. Spare yourself the stress - StudySmart!",
+        "StudyB gives students instant access to all the information they need about the best study spots on campus. Whether it's how busy a library is, what time the Hedrick Study closes, or where to reserve a YRL pod, all the answers are found in a single application that is updated in real-time. Spare yourself the stress - StudyB!",
       link: 'http://ss-temp.atwebpages.com/',
-      image: studySmartLogo
+      image: studyBLogo
+    },
+    {
+      name: 'Twain',
+      description:
+        'Twain helps users optimize planning their To Do Lists. Sign in with Google - Add a To Do List. Schedule and Optimize'
+    },
+    {
+      name: 'Twyne',
+      description:
+        "Wearables are the future. Twyne helps users interact with the world through simple and natural motions. For the first time ever, it brings automated, customizable, and continuous gesture recognition to the smartwatch. It's about treating wearables as a modality that's fundamentally different from smartphones and laptops."
     }
   ]
 }
